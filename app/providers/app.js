c2a1d0b6fa4f45245bfbd8518fe2ae554a23ca9a
Otavio Augusto <otavioaufegues@gmail.com>
2023-02-15 18:15:50 -0300

    app context

---
+++ app/providers/app.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const AppContext = React.createContext();
+
+export default AppContext;
--- app/router.js
+++ app/router.js
@@ -40,6 +40,7 @@ import ReportEvolutionScreen from './scenes/rit/reports/ReportEvolution';
 import AuthLoading from './scenes/auth/AuthLoading';
 
 import AuthProvider from './providers/auth';
+import AppProvider from './providers/app';
 
 const Stack = createStackNavigator();
 
@@ -61,183 +62,185 @@ function LogoTitle() {
 export default function Router() {
   return (
     <AuthProvider>
-      <StatusBar backgroundColor="#b22d30" barStyle="light-content" />
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Loading"
-          screenOptions={{
-            // headerTitle: props => <LogoTitle {...props} />,
-            headerStyle: {
-              backgroundColor: '#b22d30',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-              fontSize: 15,
-            },
-          }}
-        >
-          <Stack.Screen name="Loading" component={AuthLoading} />
-
-          <Stack.Screen
-            name="RegisterScreen"
-            component={RegisterScreen}
-            options={{ title: 'Cadastro de Usuário' }}
-          />
-          <Stack.Screen
-            name="LoginScreen"
-            component={LoginScreen}
-            options={{ title: '' }}
-          />
-          <Stack.Screen name="UsernameScreen" component={UsernameScreen} />
-          <Stack.Screen
-            name="ForgotPasswordScreen"
-            component={ForgotPasswordScreen}
-            options={{ title: 'Esqueci minha senha' }}
-          />
-
-          <Stack.Screen
-            name="HomeScreen"
-            component={HomeScreen}
-            options={{
-              title: 'UFJF',
-              headerTitle: (props) => (
-                <View style={formsStyles.headerTitle}>
-                  {/* <LogoTitle {...props} /> */}
-                  <Text style={formsStyles.colorWhite}>
-                    Universidade Federal de Juiz de Fora
-                  </Text>
-                </View>
-              ),
+      <AppProvider>
+        <StatusBar backgroundColor="#b22d30" barStyle="light-content" />
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Loading"
+            screenOptions={{
+              // headerTitle: props => <LogoTitle {...props} />,
+              headerStyle: {
+                backgroundColor: '#b22d30',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+                fontSize: 15,
+              },
             }}
-          />
+          >
+            <Stack.Screen name="Loading" component={AuthLoading} />
 
-          <Stack.Screen
-            name="UpdateProfileScreen"
-            component={UpdateProfileScreen}
-            options={{ title: 'Atualizar Perfil' }}
-          />
+            <Stack.Screen
+              name="RegisterScreen"
+              component={RegisterScreen}
+              options={{ title: 'Cadastro de Usuário' }}
+            />
+            <Stack.Screen
+              name="LoginScreen"
+              component={LoginScreen}
+              options={{ title: '' }}
+            />
+            <Stack.Screen name="UsernameScreen" component={UsernameScreen} />
+            <Stack.Screen
+              name="ForgotPasswordScreen"
+              component={ForgotPasswordScreen}
+              options={{ title: 'Esqueci minha senha' }}
+            />
 
-          <Stack.Screen
-            name="PITScreen"
-            component={PITScreen}
-            options={{ title: 'Plano individual de trabalho' }}
-          />
-          <Stack.Screen
-            name="UpdatePITScreen"
-            component={UpdatePITScreen}
-            options={{ title: 'Editar plano de trabalho' }}
-          />
-          <Stack.Screen
-            name="CreatePITScreen"
-            component={CreatePITScreen}
-            options={{ title: 'Cadastro de PIT' }}
-          />
-          <Stack.Screen
-            name="pitChartsScreen"
-            component={PitChartsScreen}
-            options={{ title: 'Gráficos' }}
-          />
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                title: 'UFJF',
+                headerTitle: (props) => (
+                  <View style={formsStyles.headerTitle}>
+                    {/* <LogoTitle {...props} /> */}
+                    <Text style={formsStyles.colorWhite}>
+                      Universidade Federal de Juiz de Fora
+                    </Text>
+                  </View>
+                ),
+              }}
+            />
 
-          <Stack.Screen
-            name="RITScreen"
-            component={RITScreen}
-            options={({ navigation, route }) => ({
-              title: `Atividades - ${route.params.year}`,
-              // headerRight: () => <Button onPress={() => navigation.navigate('ReportRitScreen', {year: route.params.year})} title="Relatório" color="blue" />,
-              headerRight: () => (
-                <Icon
-                  color="#fff"
-                  name="insert-chart"
-                  size={21}
-                  onPress={() =>
-                    navigation.navigate('ReportRitScreen', {
-                      year: route.params.year,
-                    })
-                  }
-                />
-              ),
-              headerRightContainerStyle: {
-                margin: 15,
-              },
-              PITScreen,
-            })}
-          />
+            <Stack.Screen
+              name="UpdateProfileScreen"
+              component={UpdateProfileScreen}
+              options={{ title: 'Atualizar Perfil' }}
+            />
 
-          <Stack.Screen
-            name="ActivityFormScreen"
-            component={ActivityFormScreen}
-            options={{ title: 'Atividade' }}
-          />
-          <Stack.Screen
-            name="ReportRitScreen"
-            component={ReportRitScreen}
-            options={({ route }) => ({
-              title: `Relatório - ${route.params.year}`,
-            })}
-          />
-          <Stack.Screen
-            name="ReportDepartmentScreen"
-            component={ReportDepartmentScreen}
-            options={({ route }) => ({
-              title: `Relatório - ${route.params.year}`,
-            })}
-          />
-          <Stack.Screen
-            name="ReportInstitutionScreen"
-            component={ReportInstitutionScreen}
-            options={({ route }) => ({
-              title: `Relatório - ${route.params.year}`,
-            })}
-          />
-          <Stack.Screen
-            name="ReportUserScreen"
-            component={ReportUserScreen}
-            options={({ route }) => ({
-              title: `Relatório - ${route.params.year}`,
-            })}
-          />
-          <Stack.Screen
-            name="ReportEvolutionScreen"
-            component={ReportEvolutionScreen}
-            options={({ route }) => ({ title: 'Relatório de Evolução' })}
-          />
+            <Stack.Screen
+              name="PITScreen"
+              component={PITScreen}
+              options={{ title: 'Plano individual de trabalho' }}
+            />
+            <Stack.Screen
+              name="UpdatePITScreen"
+              component={UpdatePITScreen}
+              options={{ title: 'Editar plano de trabalho' }}
+            />
+            <Stack.Screen
+              name="CreatePITScreen"
+              component={CreatePITScreen}
+              options={{ title: 'Cadastro de PIT' }}
+            />
+            <Stack.Screen
+              name="pitChartsScreen"
+              component={PitChartsScreen}
+              options={{ title: 'Gráficos' }}
+            />
 
-          <Stack.Screen
-            name="DepartmentScreen"
-            component={DepartmentScreen}
-            options={({ navigation, route }) => ({
-              title: `Instituição - ${route.params.year}`,
-              headerRight: () => (
-                <Icon
-                  color="#fff"
-                  name="insert-chart"
-                  size={21}
-                  onPress={() =>
-                    navigation.navigate('ReportInstitutionScreen', {
-                      year: route.params.year,
-                    })
-                  }
-                />
-              ),
-              headerRightContainerStyle: {
-                margin: 15,
-              },
-            })}
-          />
-          <Stack.Screen
-            name="InstitutionScreen"
-            component={InstitutionScreen}
-            options={{ title: 'Instituição' }}
-          />
+            <Stack.Screen
+              name="RITScreen"
+              component={RITScreen}
+              options={({ navigation, route }) => ({
+                title: `Atividades - ${route.params.year}`,
+                // headerRight: () => <Button onPress={() => navigation.navigate('ReportRitScreen', {year: route.params.year})} title="Relatório" color="blue" />,
+                headerRight: () => (
+                  <Icon
+                    color="#fff"
+                    name="insert-chart"
+                    size={21}
+                    onPress={() =>
+                      navigation.navigate('ReportRitScreen', {
+                        year: route.params.year,
+                      })
+                    }
+                  />
+                ),
+                headerRightContainerStyle: {
+                  margin: 15,
+                },
+                PITScreen,
+              })}
+            />
 
-          <Stack.Screen
-            name="ImportActivityScreen"
-            component={ImportActivityScreen}
-            options={{ title: 'Importações' }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+            <Stack.Screen
+              name="ActivityFormScreen"
+              component={ActivityFormScreen}
+              options={{ title: 'Atividade' }}
+            />
+            <Stack.Screen
+              name="ReportRitScreen"
+              component={ReportRitScreen}
+              options={({ route }) => ({
+                title: `Relatório - ${route.params.year}`,
+              })}
+            />
+            <Stack.Screen
+              name="ReportDepartmentScreen"
+              component={ReportDepartmentScreen}
+              options={({ route }) => ({
+                title: `Relatório - ${route.params.year}`,
+              })}
+            />
+            <Stack.Screen
+              name="ReportInstitutionScreen"
+              component={ReportInstitutionScreen}
+              options={({ route }) => ({
+                title: `Relatório - ${route.params.year}`,
+              })}
+            />
+            <Stack.Screen
+              name="ReportUserScreen"
+              component={ReportUserScreen}
+              options={({ route }) => ({
+                title: `Relatório - ${route.params.year}`,
+              })}
+            />
+            <Stack.Screen
+              name="ReportEvolutionScreen"
+              component={ReportEvolutionScreen}
+              options={({ route }) => ({ title: 'Relatório de Evolução' })}
+            />
+
+            <Stack.Screen
+              name="DepartmentScreen"
+              component={DepartmentScreen}
+              options={({ navigation, route }) => ({
+                title: `Instituição - ${route.params.year}`,
+                headerRight: () => (
+                  <Icon
+                    color="#fff"
+                    name="insert-chart"
+                    size={21}
+                    onPress={() =>
+                      navigation.navigate('ReportInstitutionScreen', {
+                        year: route.params.year,
+                      })
+                    }
+                  />
+                ),
+                headerRightContainerStyle: {
+                  margin: 15,
+                },
+              })}
+            />
+            <Stack.Screen
+              name="InstitutionScreen"
+              component={InstitutionScreen}
+              options={{ title: 'Instituição' }}
+            />
+
+            <Stack.Screen
+              name="ImportActivityScreen"
+              component={ImportActivityScreen}
+              options={{ title: 'Importações' }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </AppProvider>
     </AuthProvider>
   );
 }
